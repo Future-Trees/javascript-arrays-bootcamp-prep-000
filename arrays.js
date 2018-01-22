a29d6eb807c158480ba3dac5e47cf748d012a1a6
Skylar Dunn-Lubin <skylar.dunnlubin@gmail.com>
2018-01-22 20:39:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,4 +29,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   return array
 }
 
-function 
+function removeElementFromBeginningOfArray(array)
+array = array.slice(1)
+return array
