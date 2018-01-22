5b09e4e62e0794ba012a19e5684cb89d0f0fce51
Skylar Dunn-Lubin <skylar.dunnlubin@gmail.com>
2018-01-22 20:47:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -34,10 +34,14 @@ function removeElementFromBeginningOfArray(array){
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
-  return array.pop()
+  array.pop()
+
+  return array
 }
 
 function removeElementFromEndOfArray(array){
   array.slice(0,array.length-1)
+
+  return array
 }
 
